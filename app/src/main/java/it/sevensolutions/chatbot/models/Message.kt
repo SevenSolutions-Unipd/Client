877a55e2e49a3a7a4c694c962276891dbc560da3
Alessandro Cavaliere <ale.cava41@gmail.com>
2022-04-21 00:38:17 +0200

    code cleanup (ancora :-) )

--- app/src/main/java/it/sevensolutions/chatbot/models/Message.kt
+++ app/src/main/java/it/sevensolutions/chatbot/models/Message.kt
@@ -1,38 +1,13 @@
 package it.sevensolutions.chatbot.models
 
 import com.stfalcon.chatkit.commons.models.IMessage
-import com.stfalcon.chatkit.commons.models.MessageContentType
 import java.util.*
 
-class Message(private val id: String,
-              private val user: User,
-              private val text: String,
-              private val createdAt: Date) :
-    IMessage, MessageContentType.Image, MessageContentType {
+class Message(private val id: String, private val user: User, private val text: String, private val createdAt: Date) :
+    IMessage {
 
-    private var image: Image? = null
-
-    override fun getId(): String {
-        return id
-    }
-
-    override fun getText(): String {
-        return text
-    }
-
-    override fun getCreatedAt(): Date {
-        return createdAt
-    }
-
-    override fun getUser(): User {
-        return user
-    }
-
-    override fun getImageUrl(): String? {
-        return if (image == null) null else image!!.url
-    }
-
-    class Image(val url: String)
-
-
+    override fun getId(): String = id
+    override fun getText(): String = text
+    override fun getCreatedAt(): Date = createdAt
+    override fun getUser(): User = user
 }
--- app/src/main/java/it/sevensolutions/chatbot/models/User.kt
+++ app/src/main/java/it/sevensolutions/chatbot/models/User.kt
@@ -2,22 +2,9 @@ package it.sevensolutions.chatbot.models
 
 import com.stfalcon.chatkit.commons.models.IUser
 
-class User(
-    private val id: String,
-    private val name: String,
-    private val avatar: String,
-) : IUser {
+class User(private val id: String, private val name: String, private val avatar: String) : IUser {
 
-    override fun getId(): String {
-        return id
-    }
-
-    override fun getName(): String {
-        return name
-    }
-
-    override fun getAvatar(): String {
-        return avatar
-    }
-
+    override fun getId(): String = id
+    override fun getName(): String = name
+    override fun getAvatar(): String = avatar
 }
